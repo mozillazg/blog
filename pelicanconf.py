de68aa4c2326958f8a523ebead1d922cc309ad1f
Mozillazg <opensource.mozillazg@gmail.com>
2013-10-23 21:10:16 +0800

    update ansi-escape-sequences

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import logging
-
-logging.fatal('fatal')
-logging.info('info')
-logging.debug('debug')
-logging.warn('warnning')
-logging.error('error')
-
 
 """Settings for pelican."""
 
